53ef38ff86e79f1d31b92a2e1107719071dbec36
Krzysztof Karpiński <krzysztof.kaarpinski@gmail.com>
2021-12-15 21:12:37 +0100

    New methods

--- sensors.py
+++ sensors.py
@@ -1,4 +1,6 @@
 import json
+
+import matplotlib.pyplot as plt
 import requests
 import urllib
 import numpy as np
@@ -6,6 +8,7 @@ import pandas as pd
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
 pd.options.mode.chained_assignment = None
+import jupyter
 
 #Sensory
 
@@ -48,5 +51,8 @@ with open('sensory.json') as jsonFile:
     df = pd.json_normalize(jsonObject['values'])
 
     sensors = pd.DataFrame(df.groupby(['value'])['date'].sum().sort_values(ascending=False))
+    print(sensors)
+
+
 
 
